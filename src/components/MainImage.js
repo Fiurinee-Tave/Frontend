d54f15cd3ee13f92649ec8f729fa27dfd7f70bb0
Mooni <rlaansgml20@sookmyung.ac.kr>
2024-06-02 18:29:35 +0900

    vscode 확장팩 자동 포맷팅 이슈,,,

--- src/components/MainImage.js
+++ src/components/MainImage.js
@@ -1,59 +1,56 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
-import SwiperCore, {Autoplay, Navigation, Pagination} from 'swiper';
-import { Swiper,SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.min.css'
-import 'swiper/swiper.min.css'
-import 'swiper/components/navigation/navigation.min.css'
-import 'swiper/components/pagination/pagination.min.css'
+import SwiperCore, { Autoplay, Navigation, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+import "swiper/components/navigation/navigation.min.css";
+import "swiper/components/pagination/pagination.min.css";
 
 SwiperCore.use([Autoplay, Navigation, Pagination]);
 
 const StyledImg = styled.div`
-    position:relative;
-    background-size: cover;
-    width: 100vw;
-    height: 70vh;
-    display: flex;
-    align-items: center;
-
-  `;
-  const ImageSlide = styled.img`
+  position: relative;
+  background-size: cover;
+  width: 100%;
+  height: 70vh;
+  display: flex;
+  align-items: center;
+`;
+const ImageSlide = styled.img`
   width: 100vw;
   height: 70vh;
   object-fit: cover;
-  opacity:0.5;
-  filter:blur(1px);
+  opacity: 0.5;
+  filter: blur(1px);
 `;
-
 
 const LoginButton = styled.div`
   cursor: pointer;
   position: absolute;
-  top: 20px; 
+  top: 20px;
   right: 50px;
-  text-size:30px;
+  text-size: 30px;
 `;
 
 const Title = styled.div`
-  font-size:150px;
-  color:#EB5360;
+  font-size: 150px;
+  color: #eb5360;
   position: absolute;
-  top: 150px; 
+  top: 150px;
   left: 170px;
-
 `;
 
 const Detail = styled.div`
-  font-size:40px;
+  font-size: 40px;
   position: absolute;
-  top: 300px; 
+  top: 300px;
   left: 300px;
 `;
 
 const RecommendBtn = styled.button`
-  background-color:#FFF3F3;
+  background-color: #fff3f3;
   font-size: 20px;
   cursor: pointer;
   position: absolute;
@@ -61,64 +58,61 @@ const RecommendBtn = styled.button`
   bottom: 15px;
   padding: 15px 25px;
   border-style: dashed;
-  border-color:gray;
+  border-color: gray;
 `;
 
 const DetailBtn = styled.button`
-  border-radius:50px;
+  border-radius: 50px;
   font-size: 30px;
   cursor: pointer;
   position: absolute;
   right: 120px;
   bottom: 20px;
-  background-color:#FFF3F3;
+  background-color: #fff3f3;
   border-style: dashed;
-  border-color:gray;
+  border-color: gray;
 `;
 
-
-function MainImage(){
-    
-const navigate = useNavigate();
-const images = ['/img/MainImage1.png', '/img/MainImage2.png','/img/MainImage1.png','/img/MainImage2.png'];
+function MainImage() {
+  const navigate = useNavigate();
+  const images = [
+    "/img/MainImage1.png",
+    "/img/MainImage2.png",
+    "/img/MainImage1.png",
+    "/img/MainImage2.png",
+  ];
 
   const Login = () => {
     //로그인창으로 이동
     navigate("/login");
-
-};
+  };
 
   const recommend = () => {
     navigate("/reco0");
   };
 
-
-    return(
-      <StyledImg>
-           <Swiper
-                spaceBetween={50}
-                slidesPerView={1}
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
-                autoplay={{delay:3000, disableOnInteraction:false}}
-            >
-                {images.map((image, index) => (
-                    <SwiperSlide key={index}>
-                      
-                        <ImageSlide src={image} />
-                        <LoginButton onClick={Login}>로그인 / 회원가입</LoginButton>
+  return (
+    <StyledImg>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
+      >
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <ImageSlide src={image} />
+            <LoginButton onClick={Login}>로그인 / 회원가입</LoginButton>
             <RecommendBtn onClick={recommend}>선물할 꽃 추천받기</RecommendBtn>
             <Title>Fiurinee</Title>
             <Detail>당신의 마음을 선물하세요</Detail>
             <DetailBtn> ? </DetailBtn>
-            
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-            </StyledImg>
-                  
-        );
-
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </StyledImg>
+  );
 }
 
 export default MainImage;
--- src/routes/Mainpage.js
+++ src/routes/Mainpage.js
@@ -3,28 +3,26 @@ import MainImage from "../components/MainImage";
 import TodayFlower from "../components/TodayFlower";
 import FlowerShop from "../components/FlowerShop";
 
-
 const Wrapper = styled.div`
   width: 100%;
-
 `;
 
 const Line2 = styled.div`
   width: 100%;
-  display:flex;
-  justifyContent:'space-between';
-  `;
+  display: flex;
+  justifycontent: "space-between";
+`;
 
 function Mainpage() {
-  
-
-  return (<Wrapper>
-    <MainImage/>
-    <Line2>
-    <TodayFlower/>
-    <FlowerShop/>
-    </Line2>
-    </Wrapper>);
+  return (
+    <Wrapper>
+      <MainImage />
+      <Line2>
+        <TodayFlower />
+        <FlowerShop />
+      </Line2>
+    </Wrapper>
+  );
 }
 
 export default Mainpage;
