7869f35c036dc8a3179612ed100cba38aad1302b
Yeong-si <dtd1110@gmail.com>
2024-07-03 14:29:10 +0900

    #6 Feat : 추천 API 연동

--- src/routes/Recommend0.js
+++ src/routes/Recommend0.js
@@ -1,6 +1,9 @@
 import Header from "../components/Header";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import Loading from "../loading/Loading"
 
 const Wrapper = styled.div`
   width: 100vw;
@@ -42,11 +45,17 @@ const Input = styled.textarea`
   type="text";
   height:80px;
   width:63%;
-  background-color: rgba(255,255,255,0.8);
+  background-color: rgba(255,255,255,0.05);
   font-size:25px;
   text-align: center;
   border-radius:5px;
+  outline: none;
   border:none;
+  border-bottom:2px solid;
+  text-align: center;
+   font-family: "Gowun Batang", serif;
+  font-weight: 400;
+  font-style: normal;
   @media (max-width: 575px) {
     height:55px;
     font-size:15px;
@@ -61,7 +70,7 @@ const RecommendBtn = styled.button`
   padding: 18px 33px;
   border-radius:12px;
   border:none;
-    font-family: "Gowun Batang", serif;
+  font-family: "Gowun Batang", serif;
   font-weight: 400;
   font-style: normal;
   @media (max-width: 575px) {
@@ -69,29 +78,66 @@ const RecommendBtn = styled.button`
     width:70%;
     padding: 15px 5%;
     font-size: 15px;
+    
   }
 
 `;
-//
-
 
 
 function Recommend0() {
+  const [inputValue, setInputValue] = useState('');
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const recommend = () => {
     navigate("/reco1");
   };
 
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+    
+  const posetData = async () => {
+    setLoading(true);
+    try{
+      const response = await axios.post('http://3.36.169.209:8080/model/ment',
+      {
+        ment: inputValue
+      },
+      {
+         'Content-Type' : 'application/json'
+      }
+    );
+    console.log("데이터 보내기 성공1");
+    setLoading(false);
+    console.log(response); 
+
+    navigate("/reco1", {state : {
+      flower: response.data,
+      inputment : inputValue
+    }});
+    
+    }
+    catch (error) {
+      console.error("데이터 보내기 실패", error);
+    }
+  };
+
   return (
+    loading ? 
+    <Wrapper> <Header login={true} /> <Loading/> </Wrapper>:(
     <Wrapper>
-      <Header />
+      <Header login={true} />
       <Line>
         <Bigtitle>전해주고 싶은 멘트를 작성해주세요</Bigtitle>
         <Title>멘트에 어울리는 꽃 조합을 선물해드립니다.</Title>
-        <Input maxlength='50'></Input>
-        <RecommendBtn onClick={recommend}> 꽃 추천받으러 가기 ➡ </RecommendBtn>
+        <Input value={inputValue}
+          onChange={handleInputChange} maxlength='50'></Input>
+        <RecommendBtn onClick={posetData} disabled={inputValue === ''}> 꽃 추천받으러 가기 ➡ </RecommendBtn>
       </Line>
     </Wrapper>
+    )
   );
 }
 
